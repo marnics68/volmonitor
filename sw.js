62e4527f10c01e9b1d07a632b67c40ff6c14de31
marnics68 <80492590+marnics68@users.noreply.github.com>
2026-07-04 15:27:43 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "vpt-v1";
+const CACHE = "vpt-v2";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
